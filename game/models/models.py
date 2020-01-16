b953fc0170704e0f427aab66177213c71ab31ce4
Guillem <guillemclar@gmail.com>
2020-01-16 17:33:12 +0100

    Revisar BOTO en viws acabaes demo mines

--- game/models/models.py
+++ game/models/models.py
@@ -2,6 +2,7 @@
 import random
 
 from odoo import models, fields, api, tools
+from openerp.exceptions import ValidationError
 
 
 class player(models.Model):
@@ -9,17 +10,32 @@ class player(models.Model):
     name = fields.Char()
     planetes = fields.One2many('game.planets', 'player')
 
+    @api.constrains('planetes')
+    def _check_something(self):
+        for r in self:
+            if r.planetes > 10:
+                raise ValidationError("You have too many planets %s" % r.planetes)
+
+    @api.multi
+    def create_new_planet(self):
+
+        self.planetes.create({
+            'planetes': self.id
+        })
+
 
 class planets(models.Model):
     _name = 'game.planets'
     image = fields.Binary()
     # image_small = fields.Binary(string='Image', compute='_get_images', store=True)
     name = fields.Char()
-    player = fields.Many2one('game.player')
+    player = fields.Many2one('game.player', ondelete='cascade')
     flota = fields.One2many('game.fleet', 'naves')
     resources = fields.One2many('game.resource', 'planetsR')
     minas = fields.One2many('game.mines', 'planetsM')
     recursosKanban = fields.One2many(related='resources')  # Per al kanban
+    date_action = fields.Datetime('Date current action', required=False, readonly=True, select=True,
+                                  default=lambda self: fields.datetime.now())
 
 
 class fleet(models.Model):
